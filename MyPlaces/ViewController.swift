3f0235214417e9d91901704f3106082507192c5f
Aleksandr Kryukov <kryukov86@mail.ru>
2020-04-14 20:15:14 +0700

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Крюков on 14.04.2020.
+//  Copyright © 2020 Александр Крюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
